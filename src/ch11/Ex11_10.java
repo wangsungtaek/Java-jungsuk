bc4a4cf4462f8a2cdad7267a224e159876bdfdc0
Taek <68735491+wangsungtaek@users.noreply.github.com>
2020-12-06 19:47:08 -0400

    Collection

---
+++ src/ch11/Ex11_10.java
@@ -0,0 +1,18 @@
+package ch11;
+
+import java.util.*;
+
+public class Ex11_10 {
+	public static void main(String[] args) {
+		Set set = new HashSet();
+		
+		for(int i=0; set.size()<6; i++) {
+			int num = (int)(Math.random()*45) + 1;
+			set.add(new Integer(num));
+		}
+		System.out.println(set);
+		List list = new LinkedList(set);	// LinkedList(Collection c)
+		Collections.sort(list);				// Collections.sork(List list)
+		System.out.println(list);
+	}
+}
